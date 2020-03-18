f2b64166714f3172b89f95506e282d3ff74f0f96
James O'Halloran <james.p.ohalloran@gmail.com>
2020-03-18 16:27:16 -0300

    revert dynamic path for previews

--- pages/teams.tsx
+++ pages/teams.tsx
@@ -128,7 +128,7 @@ export const getStaticProps: GetStaticProps = async function({
   let teamsData = {}
   try {
     teamsData = await getJsonData(
-      preview ? 'teams.json' : 'content/pages/teams.json',
+      'content/pages/teams.json',
       sourceProviderConnection,
       accessToken
     )
